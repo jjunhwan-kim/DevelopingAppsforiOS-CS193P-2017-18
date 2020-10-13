5c322f41d20ecc53ce6606d5bf5cc90acc35e0f8
nefertirii <nefertiri972@gmail.com>
2020-10-13 22:55:06 +0900

    chapter 2

---
+++ Concentration/Concentration/Card.swift
@@ -0,0 +1,25 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by 김준환 on 2020/10/13.
+//
+
+import Foundation
+
+struct Card {
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+    
+    static var identifierFactory = 0
+    
+    static func getUniqueIdentifier() -> Int {
+        identifierFactory += 1
+        return identifierFactory
+    }
+    
+    init() {
+        self.identifier = Card.getUniqueIdentifier()
+    }
+}
