2dec49049336b37df7a0c4a88721ef85196d728a
nefertirii <nefertiri972@gmail.com>
2020-10-29 22:24:55 +0900

    Chapter 9

---
+++ Cassini/Cassini/ImageViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ImageViewController.swift
+//  Cassini
+//
+//  Created by 김준환 on 2020/10/29.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController, UIScrollViewDelegate {
+    var imageURL: URL? {
+        didSet {
+            image = nil
+            if view.window != nil { // View가 화면에 나타나면, window가 생긴다.
+                fetchImage()
+            }
+        }
+    }
+    
+    private var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        set {
+            imageView.image = newValue
+            imageView.sizeToFit()
+            scrollView.contentSize = imageView.frame.size
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if imageView.image == nil {
+            fetchImage()
+        }
+    }
+    
+    @IBOutlet var scrollView: UIScrollView! {
+        didSet {
+            scrollView.minimumZoomScale = 1/25
+            scrollView.maximumZoomScale = 1.0
+            scrollView.delegate = self
+            scrollView.addSubview(imageView)
+        }
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    
+    var imageView = UIImageView()
+    
+    private func fetchImage() {
+        if let url = imageURL {
+            let urlContents = try? Data(contentsOf: url)
+            if let imageData = urlContents {
+                image = UIImage(data: imageData)
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if imageURL == nil {
+            imageURL = DemoURLs.stanford
+        }
+    }
+}
--- Concentration/Concentration/ConcentrationThemeChooserViewController.swift
+++ Concentration/Concentration/ConcentrationThemeChooserViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ConcentrationThemeChooserViewController: UIViewController, UISplitViewControllerDelegate {
+class ConcentrationThemeChooserViewController: VCLLoggingViewController, UISplitViewControllerDelegate {
+    override var vclLoggingName: String {
+        return "ThemeChooser"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     override func awakeFromNib() {
+        super.awakeFromNib()
         splitViewController?.delegate = self
     }
     
@@ -36,19 +41,19 @@ class ConcentrationThemeChooserViewController: UIViewController, UISplitViewCont
     ]
     
     @IBAction func changeTheme(_ sender: Any) {
-        if let cvc = splitViewDetailConcentrationViewController {
-            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
-                cvc.theme = theme
-            }
-        } else if let cvc = lastSeguedToConcentrationViewController {
-            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
-                cvc.theme = theme
-            }
-            navigationController?.pushViewController(cvc, animated: true)
-        }
-        else {
+//        if let cvc = splitViewDetailConcentrationViewController {
+//            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+//                cvc.theme = theme
+//            }
+//        } else if let cvc = lastSeguedToConcentrationViewController {
+//            if let themeName = (sender as? UIButton)?.currentTitle, let theme = themes[themeName] {
+//                cvc.theme = theme
+//            }
+//            navigationController?.pushViewController(cvc, animated: true)
+//        }
+//        else {
             performSegue(withIdentifier: "Choose Theme", sender: sender)
-        }
+//        }
     }
     
     private var splitViewDetailConcentrationViewController: ConcentrationViewController? {
